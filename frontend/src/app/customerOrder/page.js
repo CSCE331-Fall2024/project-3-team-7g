d84b889db58b10abcaef8ecbfc0f39e8508621c7
DiegoLandaeta03 <diego.landaeta03@gmail.com>
2024-11-11 14:18:35 -0600

    Created start of customer order screen. Will have sizes, appetizers, and drinks that can be added to cart. A cart button will open up a modal with the current items placed in cart there.

--- frontend/src/app/customerOrder/page.js
+++ frontend/src/app/customerOrder/page.js
@@ -1,7 +1,28 @@
+// "use client";
+
+import Header from "./components/Navbar";
+import ButtonList from "../components/ButtonList";
+// import { useState } from "react";
+
 export default function Home() {
+    // const [selectedItem, setSelectedItem] = useState(null);
+
+    // // Function to handle the last clicked item
+    // const handleItemClick = (item) => {
+    //     setSelectedItem(item);
+    // };
+
     return (
-    <main className="flex-grow flex flex-col p-4">
-        <h1 className="text-3xl font-bold">Begin Order</h1>
-    </main>
+        <div className="relative flex flex-col min-h-screen bg-white">
+            <Header screen={'Begin Order'}/>
+            <main className="flex-grow flex flex-col p-4">
+                <h1 className="px-4 text-2xl font-bold">Sizes</h1>
+                <ButtonList listType="sizes"></ButtonList>
+                <h1 className="px-4 text-2xl font-bold">Appetizers</h1>
+                <ButtonList listType="appetizers"></ButtonList>
+                <h1 className="px-4 text-2xl font-bold">Drinks</h1>
+                <ButtonList listType="drinks"></ButtonList>
+            </main>
+        </div>
     );
 }
