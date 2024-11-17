b88ef93105c7537a89c33949110524b87cb31cf5
DiegoLandaeta03 <diego.landaeta03@gmail.com>
2024-11-17 15:52:31 -0600

    Navigation for different sizes is made and prints out what each item is when it is selected. The original one "adds to cart" each of the items (drinks and appetizers).

---
+++ frontend/src/app/customerOrder/plate/page.js
@@ -0,0 +1,38 @@
+"use client";
+
+import Navbar from "../components/Navbar";
+import ButtonList from "@/app/components/ButtonList";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+
+export default function plate() {
+    // const router = useRouter();
+    const [selectedItem, setSelectedItem] = useState(null);
+    const [itemType, setItemType] = useState(null);
+    // const size = router.query.size;
+    // console.log(size);
+
+    const handleItemClick = (itemType, item) => {
+        setItemType(itemType);
+        setSelectedItem(item);
+    };
+
+
+    useEffect(() => {
+        if (selectedItem != '') {
+            console.log(`Chose ${selectedItem}`);
+        }
+    }, [selectedItem]);
+
+    return (
+        <div className="relative flex flex-col min-h-screen bg-white">
+            <Navbar screen={'Choose Items'}/>
+            <main className="flex-grow flex flex-col p-4">
+                <h1 className="px-4 text-2xl font-bold">Sides</h1>
+                <ButtonList listType="sides" handleItemClick={handleItemClick}></ButtonList>
+                <h1 className="px-4 text-2xl font-bold">Entrees</h1>
+                <ButtonList listType="entrees" handleItemClick={handleItemClick}></ButtonList>
+            </main>
+        </div>
+    );
+}
