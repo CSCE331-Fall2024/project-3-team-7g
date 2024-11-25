05718b6f6d5db797fde9f2f1b0d2dfddee07c2d2
atkinsonl477 <atkinsonl477@tamu.edu>
2024-11-25 13:04:18 -0600

    Changed misc. backend functions to take an email as a parameter. Allows for the google O-Auth stuff to interface much nicer than before. Will discuss specifics with group during SCRUM today.

--- backend/index.js
+++ backend/index.js
@@ -1,40 +1,18 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const cors = require('cors')
-const login = require('./routes/login')
 const getItemsByType = require('./routes/getItemsByType')
-const addQuantity = require('./routes/Manager')
-const addMenuItem = require('./routes/Manager')
-const addInventoryItem = require('./routes/Manager')
-const getWeeklySales = require('./routes/Manager')
-const getHourlySales = require('./routes/Manager')
-const completePurchase = require('./routes/purchasing')
-const getMenuItems = require('./routes/Manager')
-const updateMenuPricing = require('./routes/Manager')
-const getMenuPrices = require('./routes/Manager')
-const getItemInventory = require('./routes/Manager')
-const getUsageData = require('./routes/Manager')
-
+const manager = require('./routes/manager')
+const purchasing = require('./routes/purchasing')
+const user = require('./routes/user')
 const app = express();
 app.use(cors())
 const port = 3000;
 
-
-
-app.use('/login', login)
 app.use('/getItemsByType', getItemsByType)
-app.use('/Manager', addQuantity)
-app.use('/Manager', addMenuItem)
-app.use('/Manager', addInventoryItem)
-app.use('/Manager', getWeeklySales)
-app.use('/Manager', getHourlySales)
-app.use('/Manager', getMenuItems)
-app.use('/Manager', updateMenuPricing)
-app.use('/purchasing', completePurchase)
-app.use('/Manager', getMenuPrices)
-app.use('/Manager', getItemInventory)
-app.use('/Manager', getUsageData)
-
+app.use('/Manager', manager)
+app.use('/purchasing', purchasing)
+app.use('/user', user)
 
 // Add process hook to shutdown pool
 process.on('SIGINT', function() {
