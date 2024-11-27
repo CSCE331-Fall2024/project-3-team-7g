9cb76dedb7a55d8796d82681d178c434a063a051
saberB99 <sbell99@tamu.edu>
2024-11-26 21:04:41 -0600

    testing render

---
+++ run-commands.bat
@@ -0,0 +1,6 @@
+
+:: Start nodemon in the current window (this will run continuously)
+start cmd /k "cd backend && nodemon"
+
+:: Start frontend npm run dev in a new terminal window
+start cmd /k "cd frontend && npm run dev"
