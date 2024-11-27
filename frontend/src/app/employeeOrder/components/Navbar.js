49537f66177c926e2bbc25b1983bd7f68a49532c
DiegoLandaeta03 <diego.landaeta03@gmail.com>
2024-11-26 19:25:51 -0600

    Removed  "Home" option in navbar for the employee order screen. This is since they technically should just be filling out the order that is being made in person.

--- frontend/src/app/employeeOrder/components/Navbar.js
+++ frontend/src/app/employeeOrder/components/Navbar.js
@@ -1,14 +1,7 @@
-import Link from "next/link";
-
 function Navbar({ screen }) {
     return (
         <nav className="bg-[var(--navbar-bg)] text-[var(--navbar-text)] h-16 flex items-center justify-between px-8">
             <h1 className="text-3xl font-bold">{screen}</h1>
-            <ul className="flex space-x-8">
-                <li>
-                    <Link href="/" className="navbarLinks">Home</Link>
-                </li>
-            </ul>
         </nav>
     );
 }
