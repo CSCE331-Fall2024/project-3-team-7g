b801bd0d3aece99a7acaa7a6aae8e232f3c48f55
Tgam3r101 <tlanclos@tamu.edu>
2024-11-26 18:12:24 -0600

    1) employeeOrdering updated with the pages.
    2) Updated Navbar in ManagerScreen to link to employeeOrdering.

--- frontend/src/app/ManagerScreen/components/Navbar.js
+++ frontend/src/app/ManagerScreen/components/Navbar.js
@@ -10,6 +10,9 @@ const Navbar = () => {
       </ul>
       <ul className="flex space-x-8">
         <li>
+          <Link href="../employeeOrder" className="navbarLinks">Order</Link>
+        </li>
+        <li>
           <Link href="/ManagerScreen/personnel" className="navbarLinks">Personnel</Link>
         </li>
         <li>
