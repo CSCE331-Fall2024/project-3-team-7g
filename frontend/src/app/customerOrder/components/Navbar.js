c82be58a9ba936ca71d1b84c96891967996ac789
Tgam3r101 <tlanclos@tamu.edu>
2024-12-09 17:00:32 -0600

    Updated the customerOrder NavBar to Log Out when clicking "Log Out"

--- frontend/src/app/customerOrder/components/Navbar.js
+++ frontend/src/app/customerOrder/components/Navbar.js
@@ -1,12 +1,20 @@
 import Link from "next/link";
 
 function Navbar({ screen }) {
+
+    const handleLogout = () => {
+        localStorage.removeItem("userEmail");
+        localStorage.removeItem("userName");
+        localStorage.removeItem("Classification");
+        router.push("/");
+    };
+
     return (
         <nav className="bg-[var(--navbar-bg)] text-[var(--navbar-text)] h-16 flex items-center justify-between px-8">
             <h1 className="text-3xl font-bold">{screen}</h1>
             <ul className="flex space-x-8">
                 <li>
-                    <Link href="/" className="navbarLinks">Home</Link>
+                    <Link href="/" className="navbarLinks" onClick={handleLogout}>Log Out</Link>
                 </li>
             </ul>
         </nav>
